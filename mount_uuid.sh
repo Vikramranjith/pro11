d1db1bdd9b13c14172510480de404a61803aba41
Vikramranjith <vikramranjith2006@gmail.com>
2026-09-23 12:43:33 +0530

    Add commands to mount filesystem by UUID

--- mount_uuid.sh
+++ mount_uuid.sh
@@ -8,21 +8,23 @@
 
 
 # Display filesystem UUID
+sudo blkid
 
 
 
 
 # Create mount directory
+sudo mkdir -p /mnt/mydata
 
 
 
 
 # Mount filesystem using UUID
 # Replace YOUR_UUID with actual UUID
+sudo mount UUID="YOUR_UUID" /mnt/mydata
 
 
 
 
 # Display mounted filesystem
-
-
+df -h /mnt/mydata
